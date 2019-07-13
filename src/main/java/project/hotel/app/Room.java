42e73fe3782106225a89756c6c572822960658ca
Gabriel Chelariu <gabrielchelariu@outlook.com>
2019-07-13 14:33:32 +0300

    Replace List in Room(class) with Set.

--- src/main/java/project/hotel/app/Room.java
+++ src/main/java/project/hotel/app/Room.java
@@ -2,42 +2,37 @@ package project.hotel.app;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Room implements Serializable {
 
     private static final long serialVersionUID = -7719649347533111463L;
     int nr;
-    List<Rezervation> rezervationList;
+    SortedSet<Rezervation> reservationSet;
 
     Room(int nr) {
         this.nr = nr;
-        rezervationList = new ArrayList<Rezervation>();
+        reservationSet = new TreeSet<>();
     }
 
     public boolean reserve(LocalDate date, String name) {
-
         Rezervation r = new Rezervation(date, name, nr);
-        if(rezervationList.contains(r)){
-            return false;
-        } else{
-            rezervationList.add(r);
-            return true;
-        }
+        return reservationSet.add(r);
     }
 
     @Override
-    public String toString(){
-        return "" + nr + rezervationList;
+    public String toString() {
+        return "" + nr + reservationSet;
     }
 
     public boolean isAvailable(LocalDate date) {
         Rezervation r = new Rezervation(date, "", nr);
-        return rezervationList.contains(r) == false;
+        return reservationSet.contains(r) == false;
     }
 
     public List<Rezervation> getAllReservations() {
-        return rezervationList;
+        List<Rezervation> x = new ArrayList<>();
+        x.addAll(reservationSet);
+        return x;
     }
 }
